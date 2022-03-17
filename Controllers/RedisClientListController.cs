dc2a91213587cd483a1820a9c4ac1aa7cd178104
Andre Luis Soares Alves Monteiro <andre.monteiro@phidelis.com.br>
2022-03-17 11:18:23 -0300

    Adicionado o controller para exemplificar o uso do redis como lista;

---
+++ Controllers/RedisClientListController.cs
@@ -0,0 +1,45 @@
+﻿using Fundamentos.Redis.Entities;
+using Fundamentos.Redis.Settings;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+
+namespace Fundamentos.Redis.Controllers
+{
+    [ApiController]
+    [Route("service-stack-redis-list")]
+    public class RedisClientListController : ControllerBase
+    {
+        private static string _listId = $"atendente:aa1e106a-4059-4a02-911e-bbc44fd79d39";
+        private readonly IRedisClient _client;
+        public RedisClientListController(
+            IOptions<RedisSettings> options)
+        {
+            _client = new RedisClient(options.Value.RedisConnection);
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var ids = _client.GetAllItemsFromList(_listId);
+            return Ok(ids);
+        }
+
+        [HttpPost]
+        public IActionResult Post(string atendimentoid)
+        {
+            _client.AddItemToList(_listId, atendimentoid);
+            return Ok(atendimentoid);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(string atendimentoid)
+        {
+            var result = _client.RemoveItemFromList(_listId, atendimentoid);
+            return Ok(result);
+        }
+    }
+}
