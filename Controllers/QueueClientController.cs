9ca0b53a85ac24984f5c98803355df8bdcb6a603
Andre Luis Soares Alves Monteiro <andre.monteiro@phidelis.com.br>
2022-05-21 16:20:27 -0300

    Implementada a extensão para trabalhar com lista no formato de filas;

---
+++ Controllers/QueueClientController.cs
@@ -0,0 +1,60 @@
+﻿using Fundamentos.Redis.Entities;
+using Fundamentos.Redis.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ServiceStack.Redis;
+using System;
+
+namespace Fundamentos.Redis.Controllers
+{
+    [ApiController]
+    [Route("service-stack-redis-queue")]
+    public class QueueClientController : ControllerBase
+    {
+        private const string queuename = "fila:confirma-email";
+        private readonly ILogger<RedisClientController> _logger;
+        private readonly IRedisClientsManager _manager;
+        public QueueClientController(
+            ILogger<RedisClientController> logger,
+            IRedisClientsManager manager)
+        {
+            _logger = logger;
+            _manager = manager;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            using (var client = _manager.GetClient())
+            {
+                //var item = client.DequeueItemFromList(queuename); //recupera o último elemento da lista
+                //var item = client.PopItemFromList(queuename); //recupera o último elemento da lista
+                //var item = client.BlockingDequeueItemFromList(queuename, TimeSpan.FromSeconds(15)); //recupera o elemento da lista esperando um determinado tempo
+                //var item = client.GetAndRemoveFirstElementFromListWithBlockWait(queuename, 10);  //recupera o elemento da lista esperando um determinado tempo
+                var item = client.GetAndRemoveFirstElementFromList(queuename);
+                return Ok(item);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post(string email)
+        {
+            using (var client = _manager.GetClient())
+            {
+                client.AddElementRightPositionList(queuename, email);
+                //client.PushItemToList(queuename, email); //adiciona o elemento a direita da lista
+                return Ok();
+            }
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(string id)
+        {
+            using (var client = _manager.GetClient())
+            {
+                client.Remove(queuename);
+                return Ok();
+            }
+        }
+    }
+}
---
+++ Extensions/RedisClientExtension.Queue.cs
@@ -0,0 +1,29 @@
+﻿using ServiceStack.Redis;
+
+namespace Fundamentos.Redis.Extensions
+{
+    public static partial class RedisClientExtension
+    {
+        public static string GetAndRemoveFirstElementFromList(this IRedisClient client, string listId)
+        {
+            var result = client.Custom("LPOP", listId);
+            return result?.Text;
+        }
+
+        public static string GetAndRemoveFirstElementFromListWithBlockWait(this IRedisClient client, string listId, int waitTime)
+        {
+            var result = client.Custom("BLPOP", listId, waitTime);
+            return result?.Children[1]?.Text;
+        }
+
+        public static void AddElementLeftPositionList(this IRedisClient client, string listId, string value)
+        {
+            client.Custom("LPUSH", listId, value); ;
+        }
+
+        public static void AddElementRightPositionList(this IRedisClient client, string listId, string value)
+        {
+            client.Custom("RPUSH", listId, value); ;
+        }
+    }
+}
